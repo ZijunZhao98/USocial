9e4dbacea314b79990bc270d1979b26046563959
zijunzhao <z.zhao@wustl.edu>
2018-10-06 10:26:35 -0500

    add routes

--- routes.js
+++ routes.js
@@ -3,6 +3,28 @@
 // Routes, with inline controllers for each route.
 var express = require('express');
 var router = express.Router();
+var User = require('./models/usermodel').User;
+var Travel = require('./models/travelmodel').Travel;
+
+router.get('/', function(req, res){
+  res.render("Hello world");
+});
+
+router.post('/login', function(req, res){
+
+});
+
+router.post('/register', function(req, res){
+
+});
+
+router.get('/list', function(req, res){
+
+});
+
+router.post('/list-creation', function(req, res){
+
+});
 
 
 module.exports = router;
