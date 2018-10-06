6ca060e2ded63b7fe7df5df4867cf4abfcced7e9
lixutao1995 <gl7021@163.com>
2018-10-06 11:54:23 -0500

    dsa

--- models/travelmodel.js
+++ models/travelmodel.js
@@ -2,9 +2,37 @@
 
 // Project model
 var mongoose = require('mongoose');
-
-var Travel = mongoose.model('Travel', {
-});
+var user=require('usermodel')
+var Travel = mongoose.model('Travel', new Schema({
+  Creator: {
+    type: Schema.Types.user.ObjectId,
+    required: true
+  },
+  Destination: {
+    type: String,
+    required: true
+  },
+  Num_of_ppl: {
+    type: Number,
+    required: true
+  },
+  From: {
+    type: Date,
+    required: true
+  },
+  To: {
+    type: Date,
+    required: true
+  },
+  Description: {
+    type: String,
+    required: true
+  },
+  Interested: {
+    type: [Schema.Types.user.ObjectId],
+    required: true
+  }
+}));
 
 module.exports = {
   Travel: Travel
--- models/usermodel.js
+++ models/usermodel.js
@@ -3,8 +3,44 @@
 // Project model
 var mongoose = require('mongoose');
 
-var User = mongoose.model('User', {
-});
+var User = mongoose.model('User', new Schema({
+  Username: {
+    type: String,
+    required: true
+  },
+  First_name: {
+    type: String,
+    required: true
+  },
+  Last_name: {
+    type: String,
+    required: true
+  },
+  Birthday: {
+    type: Date,
+    required: true
+  },
+  University: {
+    type: String,
+    required: true
+  },
+  Email: {
+    type: String,
+    required: true
+  },
+  Phone_number: {
+    type: String,
+    required: true
+  },
+  Gender: {
+    type: String,
+    required: true
+  },
+  Major: {
+    type: String,
+    required: true
+  }
+}));
 
 module.exports = {
   User: User
