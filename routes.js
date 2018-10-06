493f44659848366b2b8e841c27fb5658abb94341
lixutao1995 <gl7021@163.com>
2018-10-06 14:40:37 -0500

    dsa

--- routes.js
+++ routes.js
@@ -15,22 +15,56 @@ router.post('/login', function(req, res){
 });
 
 router.post('/register', function(req, res){
+  console.log(0);
+  req.checkBody('Username', 'please enter Username').notEmpty();
+  req.checkBody('Password', 'please enter Password').notEmpty();
+  req.checkBody('First_name', 'please enter your first name').notEmpty();
+  req.checkBody('Last_name', 'please enter your last name').notEmpty();
+  req.checkBody('Birthday', 'please enter your birthday').notEmpty();
+  req.checkBody('University', 'please enter your university').notEmpty();
+  req.checkBody('Email', 'please enter your email').notEmpty();
+  req.checkBody('Phone_number', 'please enter your phone number').notEmpty();
+  req.checkBody('Gender', 'please enter your gender').notEmpty();
+  req.checkBody('Major', 'please enter your major').notEmpty();
+  
+  var error = req.validationErrors();
 
-  var demo=new User({ 
-    
-    Username:req.body.Username,
-    Password:req.body.Password,
-    First_name: req.body.First_name,
-    Last_name: req.body.Last_name,
-    Birthday: req.body.Birthday,
-    University: req.body.University,
-    Email: req.body.Email,
-    Phone_number: req.body.Phone_number,
-    Gender: req.body.Gender,
-    Major: req.body.Major
+  if(error){
+    res.render('new',{
+      Username:req.body.Username,
+      Password:req.body.Password,
+      First_name: req.body.First_name,
+      Last_name: req.body.Last_name,
+      Birthday: req.body.Birthday,
+      University: req.body.University,
+      Email: req.body.Email,
+      Phone_number: req.body.Phone_number,
+      Gender: req.body.Gender,
+      Major: req.body.Major,
+      errors: error
     });
-    
-    
+  }else{
+    console.log(req.body.category);
+    var user=new User({ 
+      Username:req.body.Username,
+      Password:req.body.Password,
+      First_name: req.body.First_name,
+      Last_name: req.body.Last_name,
+      Birthday: req.body.Birthday,
+      University: req.body.University,
+      Email: req.body.Email,
+      Phone_number: req.body.Phone_number,
+      Gender: req.body.Gender,
+      Major: req.body.Major
+    });
+    project.save(function(err){
+      if(!err){
+        console.log('success');
+        res.redirect('/');
+      }
+    });
+  }
+  
 });
 
 router.get('/list', function(req, res){
