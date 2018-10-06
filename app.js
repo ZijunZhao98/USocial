36a6c736900689b2c991a2cfcd3cb63765f282ff
zijunzhao <z.zhao@wustl.edu>
2018-10-06 14:04:56 -0500

    change a bit of front end

--- app.js
+++ app.js
@@ -28,10 +28,10 @@ mongoose.connection.on('connected', function() {
 mongoose.connection.on('error', function(err) {
   console.log('Error connecting to MongoDb. Check MONGODB_URI in env.sh');
   console.log(err);
-  // process.exit(1);
+  process.exit(1);
 });
-//mongoose.connect(process.env.MONGODB_URI);
-mongoose.connect("mongodb://localhost:27017/exampleDb");
+mongoose.connect(process.env.MONGODB_URI);
+//mongoose.connect("mongodb://localhost:27017/exampleDb");
 
 // Handlabars setup
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
